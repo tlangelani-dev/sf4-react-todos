57689d96e0cb6b3591f6bbff4b8f770680757588
Tlangelani Mboweni <mbowenit@gmail.com>
2020-03-08 22:41:03 +0200

    ad context

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,15 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import TodoContextProvider from "./contexts/TodoContext";
 
-// any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+class App extends Component {
+    render() {
+        return (
+            <TodoContextProvider>
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+            </TodoContextProvider>
+        );
+    }
+}
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+render(<App />, document.querySelector('#root'));
